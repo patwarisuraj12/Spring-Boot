e1469a65bad9f73a69c7e69f83e0bcf116bb6ee3
Suraj Patwari <surajpatwari@Surajs-MacBook-Air.local>
2023-01-22 21:28:06 +0530

    RestTemplate used for internally communicating APIs with third party service

---
+++ Demo/src/main/java/com/library/demo/controller/ImageController.java
@@ -0,0 +1,26 @@
+package com.library.demo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ImageController {
+
+	@GetMapping(value = "/images", produces = MediaType.IMAGE_JPEG_VALUE)
+	public byte[] getImage(@RequestParam("id") int id, 
+						   @RequestParam(value = "length", required = false, defaultValue = "200") int length, 
+						   @RequestParam(value = "breadth", required = false, defaultValue = "200") int breadth) {
+		
+					String url = "https://picsum.photos/id/"+id+"/"+length+"/"+breadth;
+					byte[] image;
+					
+					RestTemplate rt = new RestTemplate();
+					
+					image = rt.getForObject(url, byte[].class);
+					
+					return image;
+	}
+}
